666ff97a5004daaa6a2b27f8793fe9c6d15e80a4
Robel Seyoum <robel_seyoum@yahoo.co.uk>
2024-06-12 18:16:55 +0100

    create a project

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.com.android.application) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.hilt.plugin) apply false
+    alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.navigation.safeargs) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
+}
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.navigationSafeArgs)
+    }
+}
